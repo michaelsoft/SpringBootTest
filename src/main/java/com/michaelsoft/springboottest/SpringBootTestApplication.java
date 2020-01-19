50c5d405479235cb39a726062ac05cb3088a7024
michaelsoft <michaellyk@qq.com>
2020-01-19 17:18:23 +0800

    More

--- src/main/java/com/michaelsoft/springboottest/SpringBootTestApplication.java
+++ src/main/java/com/michaelsoft/springboottest/SpringBootTestApplication.java
@@ -4,13 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 //@EnableAutoConfiguration
 @SpringBootApplication
 @EntityScan("com.michaelsoft.springboottest.entity")
 public class SpringBootTestApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootTestApplication.class, args);
 	}
